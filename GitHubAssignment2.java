91fdd1859cb0b6dd4d013578807d32220740bebc
Eda Eylül Aslan <eylul.aslan@ug.bilkent.edu.tr>
2023-09-20 20:13:09 +0300

    a, b, c parts of the assignment

---
+++ GitHubAssignment2.java
@@ -0,0 +1,46 @@
+/**
+ * GitHub-Assignment-2
+ */
+import java.util.Scanner;
+import java.util.Random;
+
+public class GitHubAssignment2{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Please enter size of the array: ");
+        int size = scan.nextInt();
+        int[] arr = new int[size];
+        Random rand = new Random();
+        for(int i = 0; i < size; i++){
+            int elt = rand.nextInt(101);
+            arr[i] = elt;
+        }
+        int option = 0;
+        while(option != 4){
+            System.out.println("Please enter your choice: ");
+            option = scan.nextInt();
+            if(option == 1){
+                int min = 101; int max = -1;
+                for(int i = 0; i < size; i++){
+                    if(arr[i] <= min){
+                        min = arr[i];
+                    }
+                    if(arr[i] >= max){
+                        max = arr[i];
+                    }
+                }
+            }
+            else if(option == 2){
+
+            }
+            else if(option == 3){
+
+            }
+            //Enter 4 to exit
+        }
+        scan.close();
+    }
+}
+
+    
+
